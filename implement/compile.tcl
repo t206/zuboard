8292ca0e4a198f588e30eb85a1b141c079d349fc
Your Name <you@example.com>
2023-07-04 12:02:43 -0400

    compiled with 2023.1 Vivado and Petalinux

--- implement/compile.tcl
+++ implement/compile.tcl
@@ -23,6 +23,7 @@ write_debug_probes -force ./results/top.ltx
 write_hw_platform -fixed -force -file ./results/top.xsa
 report_timing_summary   -file ./results/timing.rpt
 report_utilization      -file ./results/utilization.rpt
+report_io               -file ./results/io.rpt
 
 set_property BITSTREAM.GENERAL.COMPRESS TRUE [get_designs impl_1]
 write_bitstream -bin_file -force ./results/top.bit
