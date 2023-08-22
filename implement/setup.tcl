0458d9132bdc1104a4ffb593c28f71868ada9bf1
pete <peter.dudley@precisionplanting.com>
2023-08-22 09:49:23 -0400

    rebuilt system.tcl without board definition file, PCIe is enabled

--- implement/setup.tcl
+++ implement/setup.tcl
@@ -3,7 +3,9 @@ close_project -quiet
 file delete -force proj.xpr *.os *.jou *.log proj.srcs proj.cache proj.runs
 #
 create_project -force proj 
-set_property board_part avnet.com:zuboard_1cg:part0:1.0 [current_project]
+#set_property board_part avnet.com:zuboard_1cg:part0:1.0 [current_project]
+set_property part xczu1cg-sbva484-1-e [current_project]
+
 #set_param board.repoPaths /opt/Xilinx/Vivado/2021.1/data/boards/board_files
 #set_property board_part xilinx.com:zcu104:part0:1.0 [current_project]
 #set_property board_part xilinx.com:kv260:part0:1.1 [current_project]
